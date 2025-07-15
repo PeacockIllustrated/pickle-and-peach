9bbb169bf8e00cfd9d009a86d8efd2dbe92cb6f7
PeacockIllustrated <138880912+PeacockIllustrated@users.noreply.github.com>
2025-07-15 21:20:27 +0100

    Update firestore-mock.js

--- js/firestore-mock.js
+++ js/firestore-mock.js
@@ -9,7 +9,7 @@ export function fetchMenuItems() {
             name: "The P&P Sourdough",
             description: "Toasted sourdough with smashed avocado, pickled onions, and a sprinkle of chilli flakes.",
             tag: "Brunch",
-            image: "https://cdn.icon-icons.com/icons2/2070/PNG/512/avocado_icon_126322.png" // Avocado
+            image: "https://sdmntprukwest.oaiusercontent.com/files/00000000-1fa8-6243-92fc-b59dee1216ef/raw?se=2025-07-15T20%3A23%3A09Z&sp=r&sv=2024-08-04&sr=b&scid=f942b556-80bd-58fe-9919-8d959435e4da&skoid=82a3371f-2f6c-4f81-8a78-2701b362559b&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-07-15T07%3A48%3A20Z&ske=2025-07-16T07%3A48%3A20Z&sks=b&skv=2024-08-04&sig=K2RvxF50YLry2GKYqguYLFcdjcRVOIyt7NXrhY5ARB4%3D" // Avocado
         },
         {
             name: "Artisan Flat White",
@@ -21,25 +21,25 @@ export function fetchMenuItems() {
             name: "Peach Crumble Cake",
             description: "A slice of heaven. Soft sponge with juicy peach pieces and a crunchy crumble topping.",
             tag: "Bakery",
-            image: "https://cdn.icon-icons.com/icons2/2070/PNG/512/peach_icon_126325.png" // Peach
+            image: "https://sdmntprukwest.oaiusercontent.com/files/00000000-8288-6243-8b11-66c95fe00ca4/raw?se=2025-07-15T20%3A23%3A41Z&sp=r&sv=2024-08-04&sr=b&scid=8424a027-52e7-5f54-9977-7316853f0131&skoid=82a3371f-2f6c-4f81-8a78-2701b362559b&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-07-15T09%3A58%3A04Z&ske=2025-07-16T09%3A58%3A04Z&sks=b&skv=2024-08-04&sig=T16wzk2bh3EQ4Gfzr/bxq1%2B95avXxMWiy1sQ21pdIZE%3D" // Peach
         },
         {
             name: "The Shields Special",
             description: "A hearty sandwich with local ham, pease pudding, and a sweet pickle relish on a stottie.",
             tag: "Lunch",
-            image: "https://cdn.icon-icons.com/icons2/1301/PNG/512/if-food-c230-2427869_85675.png" // Sandwich
+            image: "https://sdmntprpolandcentral.oaiusercontent.com/files/00000000-3434-620a-b87c-ab0ac2969a1a/raw?se=2025-07-15T20%3A24%3A43Z&sp=r&sv=2024-08-04&sr=b&scid=e464d592-1296-572b-8187-02cfda4a07ad&skoid=82a3371f-2f6c-4f81-8a78-2701b362559b&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-07-15T06%3A28%3A14Z&ske=2025-07-16T06%3A28%3A14Z&sks=b&skv=2024-08-04&sig=SyhcPNdWtzh091SJWNF1G8BHDsmUw5vKqAtNOFM5NIQ%3D" // Sandwich
         },
         {
             name: "Single Origin Pour-Over",
             description: "A clean, delicate brew showcasing the unique tasting notes of our guest coffee beans.",
             tag: "Coffee",
-            image: "https://cdn.icon-icons.com/icons2/2382/PNG/512/coffee_pot_icon_144180.png" // Coffee Pot
+            image: "https://sdmntprpolandcentral.oaiusercontent.com/files/00000000-ce50-620a-b820-8881f4ab3ad2/raw?se=2025-07-15T20%3A24%3A16Z&sp=r&sv=2024-08-04&sr=b&scid=2d069dd3-d853-527b-bc73-3ea3225deabc&skoid=82a3371f-2f6c-4f81-8a78-2701b362559b&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-07-15T06%3A59%3A23Z&ske=2025-07-16T06%3A59%3A23Z&sks=b&skv=2024-08-04&sig=t/zogaAQ2T5hIRZ9GvmnVSTAmMVVD7Lik/YcwfPiHes%3D" // Coffee Pot
         },
         {
             name: "Vegan Sausage Roll",
             description: "Our homemade flaky pastry filled with a savoury herb and sausage-style filling.",
             tag: "Bakery",
-            image: "https://cdn.icon-icons.com/icons2/2157/PNG/512/wheat_icon_132203.png" // Wheat/Pastry
+            image: "https://sdmntpritalynorth.oaiusercontent.com/files/00000000-9d24-6246-84b2-e6440cca9261/raw?se=2025-07-15T20%3A25%3A05Z&sp=r&sv=2024-08-04&sr=b&scid=468ed556-7c4a-5eba-9d56-88f7ce9db6c3&skoid=82a3371f-2f6c-4f81-8a78-2701b362559b&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-07-15T05%3A52%3A21Z&ske=2025-07-16T05%3A52%3A21Z&sks=b&skv=2024-08-04&sig=0lDWYUEzx%2B3dGXUg6rr4ooyYULthntUqYDUlJbmlUew%3D" // Wheat/Pastry
         },
     ];
 
